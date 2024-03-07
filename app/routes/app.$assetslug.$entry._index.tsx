4f5fca10f798177c4d16171abf5f30793b3ce8f5
AML - A Laycock <aml@lhs.lancs.sch.uk>
2024-03-07 14:05:10 +0000

    fix: use asset field order in display

--- app/routes/app.$assetslug.$entry._index.tsx
+++ app/routes/app.$assetslug.$entry._index.tsx
@@ -1,4 +1,4 @@
-import {type LoaderFunctionArgs, json} from '@remix-run/node'
+import {type LoaderFunctionArgs, type MetaFunction, json} from '@remix-run/node'
 import {type Entry} from '@prisma/client'
 
 import {ensureUser} from '~/lib/utils/ensure-user'
@@ -6,6 +6,7 @@ import {getPrisma} from '~/lib/prisma.server'
 import {useLoaderData} from '@remix-run/react'
 import {AButton} from '~/lib/components/button'
 import {FIELDS} from '~/lib/fields/field'
+import {pageTitle} from '~/lib/utils/page-title'
 
 export const loader = async ({request, params}: LoaderFunctionArgs) => {
   const user = await ensureUser(request, 'entry:view', {
@@ -18,12 +19,29 @@ export const loader = async ({request, params}: LoaderFunctionArgs) => {
     where: {id: params.entry},
     include: {
       asset: true,
-      values: {include: {field: true}},
       passwords: {
         include: {password: {select: {id: true, title: true, username: true}}}
       }
     }
   })
+
+  const values = await prisma.$queryRaw<
+    Array<{
+      id: string
+      value: string
+      order: number
+      type: string
+      meta: string
+      fieldId: string
+      fieldName: string
+    }>
+  >`SELECT Value.id, Value.value, AssetField."order", Field.type, Field.meta, Value.fieldId, Field.name as fieldName FROM Value
+  INNER JOIN Entry ON Entry.Id = Value.entryId
+  INNER JOIN Asset on Asset.Id = Entry.assetId
+  INNER JOIN AssetField on AssetField.assetId = Asset.id AND AssetField.fieldId = Value.fieldId
+  INNER JOIN Field on Field.id = Value.fieldId
+  WHERE entryId = ${entry.id}
+  ORDER BY AssetField."order" ASC`
 
   const relations = await prisma.$queryRaw<
     Array<Entry & {value: string; slug: string; entryId: string; icon: string}>
@@ -36,19 +54,24 @@ export const loader = async ({request, params}: LoaderFunctionArgs) => {
     where: {body: {contains: params.entry}}
   })
 
-  return json({user, entry, relations, documents})
-}
-
-const AssetEntry = () => {
-  const {entry, relations, documents} = useLoaderData<typeof loader>()
-
-  const name = entry.values.reduce((n, v) => {
+  const name = values.reduce((n, v) => {
     if (n !== '') return n
 
     if (v.fieldId === entry.asset.nameFieldId) return v.value
 
     return ''
   }, '')
+
+  return json({user, entry, relations, documents, name, values})
+}
+
+export const meta: MetaFunction<typeof loader> = ({data}) => {
+  return [{title: pageTitle(data?.entry.asset.singular!, data?.name!)}]
+}
+
+const AssetEntry = () => {
+  const {entry, relations, documents, name, values} =
+    useLoaderData<typeof loader>()
 
   return (
     <div className="grid grid-cols-4 gap-4">
@@ -57,12 +80,12 @@ const AssetEntry = () => {
           <h2 className="border-b border-b-gray-200 text-xl font-light mb-4">
             {name}
           </h2>
-          {entry.values.map(({id, value, field}) => {
+          {values.map(({id, value, type, meta, fieldName}) => {
             const Field = ({value}: {value: string}) => {
-              return FIELDS[field.type].viewComponent({
+              return FIELDS[type].viewComponent({
                 value,
-                title: field.name,
-                meta: field.meta
+                title: fieldName,
+                meta: meta
               })
             }
 
