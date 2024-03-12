bc6864fdf1914aff6ea56e6fb9480388ac37b12d
AML - A Laycock <aml@lhs.lancs.sch.uk>
2024-03-12 08:13:59 +0000

    feat: add gfm support

--- app/lib/mdx.server.ts
+++ app/lib/mdx.server.ts
@@ -1,7 +1,15 @@
 import {bundleMDX} from 'mdx-bundler'
+import remarkGfm from 'remark-gfm'
 
 export const buildMDXBundle = async (mdx: string) => {
-  const {code} = await bundleMDX({source: mdx})
+  const {code} = await bundleMDX({
+    source: mdx,
+    mdxOptions: options => {
+      options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm]
+
+      return options
+    }
+  })
 
   return code
 }
