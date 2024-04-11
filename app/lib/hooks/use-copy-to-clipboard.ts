5108ffb02bd64010c75d5d9f16685001ce3a5c45
AML - A Laycock <aml@lhs.lancs.sch.uk>
2024-04-11 21:33:26 +0100

    fix(lint): add error description

--- app/lib/hooks/use-copy-to-clipboard.ts
+++ app/lib/hooks/use-copy-to-clipboard.ts
@@ -23,7 +23,7 @@ const useSetState = <T extends object>(
   initialState: T = {} as T
 ): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void] => {
   const [state, set] = useState<T>(initialState)
-  // @ts-expect-error
+  // @ts-expect-error would not compile
   const setState = useCallback(patch => {
     set(prevState =>
       Object.assign(
@@ -54,54 +54,56 @@ export const useCopyToClipboard = (): [
     noUserInteraction: true
   })
 
-  // @ts-expect-error
-  const copyToClipboard = useCallback(value => {
-    if (!isMounted()) {
-      return
-    }
-    let noUserInteraction
-    let normalizedValue
-    try {
-      // only strings and numbers casted to strings can be copied to clipboard
-      if (typeof value !== 'string' && typeof value !== 'number') {
-        const error = new Error(
-          `Cannot copy typeof ${typeof value} to clipboard, must be a string`
-        )
-        if (process.env.NODE_ENV === 'development') console.error(error)
-        setState({
-          value,
-          error,
-          noUserInteraction: true
-        })
+  const copyToClipboard = useCallback(
+    (value: string) => {
+      if (!isMounted()) {
         return
       }
-      // empty strings are also considered invalid
-      else if (value === '') {
-        const error = new Error(`Cannot copy empty string to clipboard.`)
-        if (process.env.NODE_ENV === 'development') console.error(error)
+      let noUserInteraction
+      let normalizedValue
+      try {
+        // only strings and numbers casted to strings can be copied to clipboard
+        if (typeof value !== 'string' && typeof value !== 'number') {
+          const error = new Error(
+            `Cannot copy typeof ${typeof value} to clipboard, must be a string`
+          )
+          if (process.env.NODE_ENV === 'development') console.error(error)
+          setState({
+            value,
+            error,
+            noUserInteraction: true
+          })
+          return
+        }
+        // empty strings are also considered invalid
+        else if (value === '') {
+          const error = new Error(`Cannot copy empty string to clipboard.`)
+          if (process.env.NODE_ENV === 'development') console.error(error)
+          setState({
+            value,
+            error,
+            noUserInteraction: true
+          })
+          return
+        }
+        normalizedValue = value.toString()
+        noUserInteraction = writeText(normalizedValue)
         setState({
-          value,
-          error,
-          noUserInteraction: true
+          value: normalizedValue,
+          error: undefined,
+          noUserInteraction
         })
-        return
+      } catch (error) {
+        setState({
+          value: normalizedValue,
+          // @ts-expect-error would not compile
+          error,
+          noUserInteraction
+        })
       }
-      normalizedValue = value.toString()
-      noUserInteraction = writeText(normalizedValue)
-      setState({
-        value: normalizedValue,
-        error: undefined,
-        noUserInteraction
-      })
-    } catch (error) {
-      setState({
-        value: normalizedValue,
-        // @ts-expect-error
-        error,
-        noUserInteraction
-      })
-    }
-  }, [])
+    },
+    [setState, isMounted]
+  )
 
   return [state, copyToClipboard]
 }
