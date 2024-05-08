384c218eeecccbb60d0ff86aa64f03afc469bbee
AML - A Laycock <aml@lhs.lancs.sch.uk>
2024-05-08 09:51:04 +0100

    feat: add links to headings in markdown

--- app/lib/mdx.server.ts
+++ app/lib/mdx.server.ts
@@ -1,11 +1,18 @@
 import {bundleMDX} from 'mdx-bundler'
 import remarkGfm from 'remark-gfm'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
 
 export const buildMDXBundle = async (mdx: string) => {
   const {code} = await bundleMDX({
     source: mdx,
     mdxOptions: options => {
       options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm]
+      options.rehypePlugins = [
+        ...(options.rehypePlugins ?? []),
+        rehypeSlug,
+        rehypeAutolinkHeadings
+      ]
 
       return options
     }
