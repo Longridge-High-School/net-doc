7b7f7567046dddbccc9943c6ec47b087104804da
AML - A Laycock <aml@lhs.lancs.sch.uk>
2024-07-23 10:53:25 +0100

    test: finish pin tests

--- tests/unit/pins.spec.ts
+++ tests/unit/pins.spec.ts
@@ -4,10 +4,15 @@ import {userForTest, appRequest} from 'tests/unit-utils'
 
 import {action as pinAction} from '~/routes/api.pin'
 
+import {getPrisma} from '~/lib/prisma.server'
+
 describe('Pins', () => {
   test('It should pin items', async () => {
+    const prisma = getPrisma()
+
     const userOne = await userForTest({role: 'reader'})
-    const userTwo = await userForTest({role: 'reader'})
+
+    const pinCount = await prisma.pin.count()
 
     const response = await pinAction({
       request: appRequest('/api/pin', {
@@ -20,5 +25,19 @@ describe('Pins', () => {
     })
 
     expect(response.status).toBe(200)
+    expect(await prisma.pin.count()).toBe(pinCount + 1)
+
+    const deleteResponse = await pinAction({
+      request: appRequest('/api/pin', {
+        method: 'POST',
+        body: JSON.stringify({target: 'documents', targetId: 'fake-uuid'}),
+        headers: await userOne.sessionHeader()
+      }),
+      context: {},
+      params: {}
+    })
+
+    expect(deleteResponse.status).toBe(200)
+    expect(await prisma.pin.count()).toBe(pinCount)
   })
 })
